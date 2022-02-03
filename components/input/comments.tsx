d759aa599b4894b560794b7a3a00ac5361427e72
Masayuki Onishi <omccello@gmail.com>
2022-02-04 01:10:51 +0900

    lesson172

--- components/input/comments.tsx
+++ components/input/comments.tsx
@@ -1,8 +1,9 @@
-import { useEffect, useState, VFC } from 'react';
+import { useContext, useEffect, useState, VFC } from 'react';
 
 import { CommentList } from './comment-list';
 import { NewComment } from './new-comment';
 import classes from './comments.module.css';
+import { NotificationContext } from '../../store/notification-context';
 
 type Props = {
   eventId: string;
@@ -13,9 +14,13 @@ export const Comments: VFC<Props> = (props) => {
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState<Required<CommentData>[]>([]);
+  const [isFetchingComments, setIsFetchingComments] = useState(false);
+
+  const notificationCtx = useContext(NotificationContext);
 
   useEffect(() => {
     if (showComments) {
+      setIsFetchingComments(true);
       fetch('/api/comments/' + eventId, {
         method: 'GET',
         headers: {
@@ -23,7 +28,10 @@ export const Comments: VFC<Props> = (props) => {
         },
       })
         .then((res) => res.json())
-        .then((data) => setComments(data.comments));
+        .then((data) => {
+          setIsFetchingComments(false);
+          setComments(data.comments);
+        });
     }
   }, [showComments]);
 
@@ -32,6 +40,12 @@ export const Comments: VFC<Props> = (props) => {
   }
 
   function addCommentHandler(commentData: CommentData) {
+    notificationCtx.showNotification({
+      title: 'Sending comment...',
+      message: 'Your comment is currently being stored into a database.',
+      status: 'pending',
+    });
+
     fetch('/api/comments/' + eventId, {
       method: 'POST',
       body: JSON.stringify(commentData),
@@ -40,14 +54,29 @@ export const Comments: VFC<Props> = (props) => {
       },
     })
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        notificationCtx.showNotification({
+          title: 'Success!',
+          message: 'Your comment was saved!',
+          status: 'success',
+        });
+        console.log(data);
+      })
+      .catch((error) => {
+        notificationCtx.showNotification({
+          title: 'Error!',
+          message: error.message || 'Something went wrong!',
+          status: 'error',
+        });
+      });
   }
 
   return (
     <section className={classes.comments}>
       <button onClick={toggleCommentsHandler}>{showComments ? 'Hide' : 'Show'} Comments</button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments} />}
+      {showComments && !isFetchingComments && <CommentList items={comments} />}
+      {showComments && isFetchingComments && <p>Loading...</p>}
     </section>
   );
 };
